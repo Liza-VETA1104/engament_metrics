b0e95e7c138447dd3ec216de59653e7fa77e93f3
Liza-VETA1104 <emakovtseva@gmail.com>
2025-12-14 01:03:16 +0400

    Update sticky_factor.sql

--- sticky_factor.sql
+++ sticky_factor.sql
@@ -1,24 +1,29 @@
 with dau as(
-	select session_start::date as day, 		
-	count(distinct user_id) user_cnt
-from user_sessions us  
-group by us.session_start::date
+	select 
+		session_start::date as day, 		
+		count(distinct user_id) user_cnt
+from 
+	user_sessions us  
+group by
+	us.session_start::date
 ),
 avg_dau as (
-select
-	to_char(day, 'YYYY-MM') as months,
-	avg(user_cnt) as dau
+	select
+		to_char(day, 'YYYY-MM') as months,
+		avg(user_cnt) as dau
 from
 	dau
 group by
 	to_char(day, 'YYYY-MM')
 	),
 mau as(
-select 
-	to_char(session_start, 'YYYY-MM') as months, 
-	count(distinct user_id) as mau
-from user_sessions
-group by to_char(session_start, 'YYYY-MM')
+	select 
+		to_char(session_start, 'YYYY-MM') as months, 
+		count(distinct user_id) as mau
+from 
+	user_sessions
+group by
+	to_char(session_start, 'YYYY-MM')
 )
 select
 	m.months,
